e9772cba02cfec0d04890b035f8c724ba716e7d6
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-02-25 10:36:38 +0600

    react core part

--- src/App.js
+++ src/App.js
@@ -1,54 +1,111 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const personName = ['Minhaj Sadik', 'Joynul Hussain', 'Iqra Aziz', 'Anika Akter', 'Isheta'];
-  const foods = ['Tea', 'Tea', 'orange', 'mango', 'Fruits']
+  const persons = [
+    {name:'Minhaj', job: 'false', food: 'mango'},
+    {name:'Joynul', job: 'true', food: 'Tea'},
+    {name:'Ab:Shukkur', job: 'true', food: 'Unknown'},
+    {name:'Alina', job: 'HouseWife', food: 'Something'}
+  ];
 
   const products = [
     {name: 'PhotoShop', price:'$80.80'},
     {name: 'illustrator', price:'$809.8' },
     {name: 'LightStrom', price:'$809.80'}
-
   ]
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>I Create A React Apps</p>
-        <Product name={products[0].name} price={products[0].price}></Product>
-        <Product name={products[1].name} price={products[1].price}></Product>
-        <Product name={products[2].name} price={products[2].price}></Product>
-        
-
-        <Person name= {personName[0]} food= {foods[0]} Job='Programming' ></Person>
-        <Person name= {personName[1]} food= {foods[1]} Job='Shop'></Person>
-        <Person name= {personName[2]} food= {foods[2]} Job='Student'></Person>
-        <Person name= {personName[3]} food= {foods[3]} Job='Student'></Person>
-        <Person name= {personName[4]} food= {foods[4]} Job='Student'></Person>
+        <Counter></Counter>
+        <Users></Users>
+        {/* <ul>
+        {
+          persons.map(person=><ul><li>{person}</li></ul>)
+        }
+        {
+          products.map(product => <li>{product.name}</li> )
+        }
+        </ul> */}
+        {
+          products.map(pd => <Product product={pd}></Product>)
+        }
+        {
+          persons.map(per => <Person person={per}></Person>)
+        }
       </header>
     </div>
   );
 }
 
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handler = () => setCount(count + 1);
+  return (
+    <div>
+      <h1>Count:{count} </h1>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onMouseMove={handler}>Increase</button>
+    </div>
+  )
+}
+
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data =>setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user =><li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
 function Product(props) {
-  const productStyle = { border: '1px solid gray', margin: '10px', padding: '10px', borderRadius: '10px'}
+  const productStyle = {
+    border: '1px solid gray',
+    margin: '10px',
+    padding: '10px',
+    borderRadius: '10px'
+    }
+  const {name, price} = props.product;
   return (<div style={productStyle}>
-    <h2>{props.name} </h2>
-    <h1>{props.price}</h1>
+    <h2>{name} </h2>
+    <h1>{price}</h1>
     <button>Buy Now</button>
   </div>
   )
 }
 
 function Person(props){
-  const personStyle ={ border: '1px solid yellow', margin: '10px', padding: '10px',borderRadius: '10px'}
-  return (
-    <div style={{border: '1px solid red', boxShadow:'5px 5px 5px green', borderRadius: '10px', margin: '10px', padding: '10px'}}>
-    <h2 style={personStyle}>Name: {props.name} </h2>
-    <h3>Job: {props.Job}</h3>
-    <h4>Food: {props.food}</h4>
+  const personStyle ={ 
+    border: '1px solid yellow', 
+    margin: '10px', 
+    padding: '10px',
+    borderRadius: '10px'
+  }
+  const {name, job, food} =props.person;
+  return (<div style={
+      {border: '1px solid red', 
+      boxShadow:'5px 5px 5px green', 
+      borderRadius: '10px', 
+      margin: '10px', 
+      padding: '10px'}}>
+
+    <h2 style={personStyle}>Name:{name} </h2>
+    <h3>Job: {job}</h3>
+    <h4>Food: {food}</h4>
   </div>
   )
 }
